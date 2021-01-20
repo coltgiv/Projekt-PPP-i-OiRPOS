2bce549afb7dee9d19e5bdaac7e469186715c492
Coltgiv <coltgiv@gmail.com>
2021-01-20 16:40:24 +0100

    Set up simple collisions with walls. Changed movement to keys.

--- Projekt/Projekt.py
+++ Projekt/Projekt.py
@@ -1,9 +1,13 @@
 import arcade
 import random
+import os
 
-SPRITE_SCALING_PLAYER = 0.3
+SPRITE_SCALING_PLAYER = 0.2
 SPRITE_SCALING_COIN = 0.25
+SPRITE_SCALING_WALL = 0.5
 COIN_COUNT = 50
+
+MOVEMENT_SPEED = 5
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -23,7 +27,7 @@ class MyGame(arcade.Window):
         # Player info and engine
         self.player_sprite = None
         self.score = 0
-       # self.physics_engine = None
+        self.physics_engine = None
 
         # Don't show the mouse cursor
         self.set_mouse_visible(False)
@@ -36,11 +40,12 @@ class MyGame(arcade.Window):
         self.coin_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
 
-        self.player_sprite = arcade.Sprite("Assets/Character/Idle (1).png", SPRITE_SCALING_PLAYER)
+        self.player_sprite = arcade.Sprite("Assets/Character/Idle.png", SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
+        self.player_sprite.center_y = 64
         self.player_list.append(self.player_sprite)
 
+        #Create random coins
         for i in range(COIN_COUNT):
             coin = arcade.Sprite("Assets/Miscellaneous/coin.png", SPRITE_SCALING_COIN)
             coin.center_x = random.randrange(SCREEN_WIDTH)
@@ -48,19 +53,47 @@ class MyGame(arcade.Window):
 
             self.coin_list.append(coin)
 
-        #self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+        #Create walls just for testing
+        for x in range(173, 650, 64):
+            wall = arcade.Sprite("Assets/Miscellaneous/tile.png", SPRITE_SCALING_WALL)
+            wall.center_x = x
+            wall.center_y = 200
+            self.wall_list.append(wall)
+
+        for y in range(273, 500, 64):
+            wall = arcade.Sprite("Assets/Miscellaneous/tile.png", SPRITE_SCALING_WALL)
+            wall.center_x = 465
+            wall.center_y = y
+            self.wall_list.append(wall)
+        
+        #Set up collisions between player and walls
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
 
     def on_draw(self):
         arcade.start_render()
         self.coin_list.draw()
+        self.wall_list.draw()
         self.player_list.draw()
 
         output = f"Score: {self.score}"
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
 
-    def on_mouse_motion(self, x, y, dx, dy):
-        self.player_sprite.center_x = x
-        self.player_sprite.center_y = y
+    
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
 
     def update(self, delta_time):
         self.coin_list.update()
@@ -70,6 +103,8 @@ class MyGame(arcade.Window):
         for coin in coin_hit_list:
             coin.remove_from_sprite_lists()
             self.score +=1
+
+        self.physics_engine.update()
 
 def main():
     game = MyGame()
