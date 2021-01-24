9e56efc75a4ce6d39817b96fd6e0d1b0783ab274
Coltgiv <coltgiv@gmail.com>
2021-01-24 13:48:53 +0100

    Added tmx map. Implemented reading tmx map and sprite lists from it. Changed gravity scale and character scale.

---
+++ Projekt/Assets/Maps/Tiles.tsx
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Tiles" tilewidth="1024" tileheight="768" tilecount="32" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="PNG/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="PNG/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="PNG/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="129" height="129" source="PNG/4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="PNG/5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="PNG/6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="PNG/7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="PNG/8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="PNG/9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="PNG/10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="PNG/11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="PNG/12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="93" source="PNG/13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="93" source="PNG/14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="93" source="PNG/15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="PNG/16.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="99" source="PNG/17.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="PNG/18.png"/>
+ </tile>
+ <tile id="20">
+  <image width="50" height="41" source="PNG/Mushroom_2.png"/>
+ </tile>
+ <tile id="21">
+  <image width="90" height="54" source="PNG/Stone.png"/>
+ </tile>
+ <tile id="22">
+  <image width="116" height="44" source="PNG/Tree_1.png"/>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="PNG/Crate.png"/>
+ </tile>
+ <tile id="25">
+  <image width="133" height="65" source="PNG/Bush (1).png"/>
+ </tile>
+ <tile id="26">
+  <image width="133" height="65" source="PNG/Bush (2).png"/>
+ </tile>
+ <tile id="27">
+  <image width="73" height="47" source="PNG/Bush (3).png"/>
+ </tile>
+ <tile id="28">
+  <image width="73" height="46" source="PNG/Bush (4).png"/>
+ </tile>
+ <tile id="29">
+  <image width="62" height="64" source="PNG/Sign_2.png"/>
+ </tile>
+ <tile id="30">
+  <image width="282" height="301" source="PNG/Tree_2.png"/>
+ </tile>
+ <tile id="31">
+  <image width="282" height="275" source="PNG/Tree_3.png"/>
+ </tile>
+ <tile id="32">
+  <image width="49" height="41" source="PNG/Mushroom_1.png"/>
+ </tile>
+ <tile id="34">
+  <image width="122" height="129" source="PNG/coin.png"/>
+ </tile>
+ <tile id="36">
+  <image width="1024" height="768" source="PNG/BackGround.png"/>
+ </tile>
+</tileset>
--- Projekt/Projekt.py
+++ Projekt/Projekt.py
@@ -2,16 +2,16 @@ import arcade
 import random
 import os
 
-SPRITE_SCALING_PLAYER = 0.2
+SPRITE_SCALING_PLAYER = 0.15
 SPRITE_SCALING_COIN = 0.25
 SPRITE_SCALING_WALL = 0.5
 COIN_COUNT = 50
-GRAVITY = .45
+GRAVITY = .15
 
 MOVEMENT_SPEED = 5
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 512
 SCREEN_TITLE = "Projekt"
 
 class MyGame(arcade.Window):
@@ -24,6 +24,10 @@ class MyGame(arcade.Window):
         self.player_list = None
         self.coin_list = None
         self.wall_list = None
+        self.finish_list = None
+        self.deadly_objects = None
+        self.background_objects = None
+        self.background_images = None
 
         # Player info and engine
         self.player_sprite = None
@@ -40,40 +44,36 @@ class MyGame(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
+        self.finish_list = arcade.SpriteList()
+        self.deadly_objects = arcade.SpriteList()
+        self.background_objects = arcade.SpriteList()
+        self.background_images = arcade.SpriteList()
 
         self.player_sprite = arcade.Sprite("Assets/Character/Idle.png", SPRITE_SCALING_PLAYER)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 64
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 192
         self.player_list.append(self.player_sprite)
 
-        #Create random coins
-        for i in range(COIN_COUNT):
-            coin = arcade.Sprite("Assets/Miscellaneous/coin.png", SPRITE_SCALING_COIN)
-            coin.center_x = random.randrange(SCREEN_WIDTH)
-            coin.center_y = random.randrange(SCREEN_HEIGHT)
+        my_map = arcade.tilemap.read_tmx("Assets/Maps/FirstLevel.tmx")
 
-            self.coin_list.append(coin)
+        self.wall_list = arcade.tilemap.process_layer(my_map, "Ground", 0.5)
+        self.coin_list = arcade.tilemap.process_layer(my_map, "Coins", 0.5)
+        self.finish_list = arcade.tilemap.process_layer(my_map, "Finish", 0.5)
+        self.deadly_objects = arcade.tilemap.process_layer(my_map, "DeadlyObjects", 0.5)
+        self.background_objects = arcade.tilemap.process_layer(my_map, "BackgroundObjects", 0.5)
+        self.background_images = arcade.tilemap.process_layer(my_map, "Background", 0.5)
 
-        #Create walls just for testing
-        for x in range(0, 650, 64):
-            wall = arcade.Sprite("Assets/Miscellaneous/tile.png", SPRITE_SCALING_WALL)
-            wall.center_x = x
-            wall.center_y = 0
-            self.wall_list.append(wall)
-
-        for y in range(273, 500, 64):
-            wall = arcade.Sprite("Assets/Miscellaneous/tile.png", SPRITE_SCALING_WALL)
-            wall.center_x = 465
-            wall.center_y = y
-            self.wall_list.append(wall)
-        
         #Set up collisions between player and walls
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, gravity_constant=GRAVITY)
 
     def on_draw(self):
         arcade.start_render()
-        self.coin_list.draw()
+        self.background_images.draw()
+        self.background_objects.draw()
+        self.deadly_objects.draw()
         self.wall_list.draw()
+        self.finish_list.draw()
+        self.coin_list.draw()
         self.player_list.draw()
 
         output = f"Score: {self.score}"
